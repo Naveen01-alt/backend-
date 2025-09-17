3b33e208b9305ea3dd3253b5f5ab7499707844d2
Naveen01-alt <CHAMAKURINAVEEN11@GMAIL.COM>
2025-09-17 15:48:24 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,8 @@ const app = express();
 // ✅ Update allowed origins
 const allowedOrigins = [
   "https://milk-mart-5auf.vercel.app"  ,
-  "https://naveen-sparkle-portfolio.vercel.app"
+  "https://naveen-sparkle-portfolio.vercel.app",
+  "https://vinaybyagari-portfolio.vercel.app"
 ];
 
 app.use(cors({
@@ -93,7 +94,38 @@ app.post("/send-message", async (req, res) => {
   }
 });
 
+app.post("/send-message1", async (req, res) => {
+  const { name, email, message } = req.body;
 
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+    to: "vinaybyagari07@gmail.com", // you receive it
+    subject: "New Portfolio Contact Message",
+    text: `
+📩 New Message Received:
+
+👤 Name: ${name}
+📧 Email: ${email}
+💬 Message: ${message}
+    `.trim(),
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Message sent successfully!" });
+  } catch (error) {
+    console.error("❌ Failed to send message:", error);
+    res.status(500).json({ error: "Message sending failed" });
+  }
+});
 
 
 
